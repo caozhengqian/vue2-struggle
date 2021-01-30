bace38791884d8efb4b2ac4110cc918169a44157
1264419544@qq.com <1264419544@qq.com>
2021-01-30 23:16:16 +0800

    Home2

--- src/views/HomeList/Home1.vue
+++ src/views/HomeList/Home1.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="Home1">
     <p>Home1</p>
+    <Home2/>
   </div>
 </template>
 
 <script>
-import { home } from "../../api/request";
+import {home} from "../../api/request";
+import Home2 from './Home2'
+
 export default {
   components: {
-    //       All,
+    Home2
   },
   name: "Home1",
   props: {
@@ -23,77 +26,78 @@ export default {
   beforeCreate() {
     console.log(`%c Home1-->beforeCreate`, "color:#FF9797");
     setTimeout(() => {
-      console.log(`%c Home1------>beforeCreate2秒`, "color:#CE0000");
+      console.log(`%c Home1------------>beforeCreate2秒`, "color:#CE0000");
     }, 2000);
     home(2000).then(() => {
-      console.log(`%c Home1---------->beforeCreate接口2秒`, "color:#600000");
+      console.log(`%c Home1-------------------------->beforeCreate接口2秒`, "color:#600000");
     });
   },
   created() {
     console.log(`%c Home1-->created`, "color:#FF9797");
     setTimeout(() => {
-      console.log(`%c Home1------>created2秒`, "color:#CE0000");
+      console.log(`%c Home1------------>created2秒`, "color:#CE0000");
     }, 2000);
     home(5000).then(() => {
-      console.log(`%c Home1---------->created5秒接口`, "color:#600000");
+      console.log(`%c Home1-------------------------->created5秒接口`, "color:#600000");
     });
   },
   beforeMount() {
     console.log(`%c Home1-->beforeMount`, "color:#FF9797");
     setTimeout(() => {
-      console.log(`%c Home1------>beforeMount2秒`, "color:#CE0000");
+      console.log(`%c Home1------------>beforeMount2秒`, "color:#CE0000");
     }, 2000);
     home(2000).then(() => {
-      console.log(`%c Home1---------->beforeMount接口2秒`, "color:#600000");
+      console.log(`%c Home1-------------------------->beforeMount接口2秒`, "color:#600000");
     });
   },
   mounted() {
     console.log(`%c Home1-->mounted`, "color:#FF9797");
     setTimeout(() => {
-      console.log(`%c Home1------>mounted2秒`, "color:#CE0000");
+      console.log(`%c Home1------------>mounted2秒`, "color:#CE0000");
     }, 2000);
     home(2000).then(() => {
-      console.log(`%c Home1---------->mounted接口2秒`, "color:#600000");
+      console.log(`%c Home1-------------------------->mounted接口2秒`, "color:#600000");
     });
   },
   beforeUpdate() {
     console.log(`%c Home1-->beforeUpdate`, "color:#FF9797");
     setTimeout(() => {
-      console.log(`%c Home1------>beforeUpdate2秒`, "color:#CE0000");
+      console.log(`%c Home1------------>beforeUpdate2秒`, "color:#CE0000");
     }, 2000);
     home(2000).then(() => {
-      console.log(`%c Home1---------->beforeUpdate接口2秒`, "color:#600000");
+      console.log(`%c Home1-------------------------->beforeUpdate接口2秒`, "color:#600000");
     });
   },
   updated() {
     console.log(`%c Home1-->updated`, "color:#FF9797");
     setTimeout(() => {
-      console.log(`%c Home1------>updated2秒`, "color:#CE0000");
+      console.log(`%c Home1------------>updated2秒`, "color:#CE0000");
     }, 2000);
     home(2000).then(() => {
-      console.log(`%c Home1---------->updated接口2秒`, "color:#600000");
+      console.log(`%c Home1-------------------------->updated接口2秒`, "color:#600000");
     });
   },
   beforeDestroy() {
     console.log(`%c Home1-->beforeDestroy`, "color:#FF9797");
     setTimeout(() => {
-      console.log(`%c Home1------>beforeDestroy2秒`, "color:#CE0000");
+      console.log(`%c Home1------------>beforeDestroy2秒`, "color:#CE0000");
     }, 2000);
     home(2000).then(() => {
-      console.log(`%c Home1---------->beforeDestroy接口2秒`, "color:#600000");
+      console.log(`%c Home1-------------------------->beforeDestroy接口2秒`, "color:#600000");
     });
   },
   destroyed() {
     console.log(`%c Home1-->destroyed`, "color:#FF9797");
     setTimeout(() => {
-      console.log(`%c Home1------>destroyed2秒`, "color:#CE0000");
+      console.log(`%c Home1------------>destroyed2秒`, "color:#CE0000");
     }, 2000);
     home(2000).then(() => {
-      console.log(`%c Home1---------->destroyed接口2秒`, "color:#600000");
+      console.log(`%c Home1-------------------------->destroyed接口2秒`, "color:#600000");
     });
   },
   methods: {
-    aa() {}
+    aa() {
+    }
   }
 };
 </script>
--- src/views/HomeList/Home2.vue
+++ src/views/HomeList/Home2.vue
@@ -7,6 +7,8 @@
 <script>
 //import { mapState } from "vuex";
 //import All from "./comLife/All";
+import {home} from "@/api/request";
+
 export default {
   components: {
     //       All,
@@ -21,7 +23,79 @@ export default {
   data() {
     return {};
   },
-  created() {},
+
+  beforeCreate() {
+    console.log(`%c      Home2-->beforeCreate`, "color:#afb4db");
+    setTimeout(() => {
+      console.log(`%c      Home2------------>beforeCreate2秒`, "color:#4e72b8");
+    }, 2000);
+    home(2000).then(() => {
+      console.log(`%c      Home2-------------------------->beforeCreate接口2秒`, "color:#181d4b");
+    });
+  },
+  created() {
+    console.log(`%c      Home2-->created`, "color:#afb4db");
+    setTimeout(() => {
+      console.log(`%c      Home2------------>created2秒`, "color:#4e72b8");
+    }, 2000);
+    home(5000).then(() => {
+      console.log(`%c      Home2-------------------------->created5秒接口`, "color:#181d4b");
+    });
+  },
+  beforeMount() {
+    console.log(`%c      Home2-->beforeMount`, "color:#afb4db");
+    setTimeout(() => {
+      console.log(`%c      Home2------------>beforeMount2秒`, "color:#4e72b8");
+    }, 2000);
+    home(2000).then(() => {
+      console.log(`%c      Home2-------------------------->beforeMount接口2秒`, "color:#181d4b");
+    });
+  },
+  mounted() {
+    console.log(`%c      Home2-->mounted`, "color:#afb4db");
+    setTimeout(() => {
+      console.log(`%c      Home2------------>mounted2秒`, "color:#4e72b8");
+    }, 2000);
+    home(2000).then(() => {
+      console.log(`%c      Home2-------------------------->mounted接口2秒`, "color:#181d4b");
+    });
+  },
+  beforeUpdate() {
+    console.log(`%c      Home2-->beforeUpdate`, "color:#afb4db");
+    setTimeout(() => {
+      console.log(`%c      Home2------------>beforeUpdate2秒`, "color:#4e72b8");
+    }, 2000);
+    home(2000).then(() => {
+      console.log(`%c      Home2-------------------------->beforeUpdate接口2秒`, "color:#181d4b");
+    });
+  },
+  updated() {
+    console.log(`%c      Home2-->updated`, "color:#afb4db");
+    setTimeout(() => {
+      console.log(`%c      Home2------------>updated2秒`, "color:#4e72b8");
+    }, 2000);
+    home(2000).then(() => {
+      console.log(`%c      Home2-------------------------->updated接口2秒`, "color:#181d4b");
+    });
+  },
+  beforeDestroy() {
+    console.log(`%c      Home2-->beforeDestroy`, "color:#afb4db");
+    setTimeout(() => {
+      console.log(`%c      Home2------------>beforeDestroy2秒`, "color:#4e72b8");
+    }, 2000);
+    home(2000).then(() => {
+      console.log(`%c      Home2-------------------------->beforeDestroy接口2秒`, "color:#181d4b");
+    });
+  },
+  destroyed() {
+    console.log(`%c      Home2-->destroyed`, "color:#afb4db");
+    setTimeout(() => {
+      console.log(`%c      Home2------------>destroyed2秒`, "color:#4e72b8");
+    }, 2000);
+    home(2000).then(() => {
+      console.log(`%c      Home2-------------------------->destroyed接口2秒`, "color:#181d4b");
+    });
+  },
   methods: {
     aa() {}
   }
