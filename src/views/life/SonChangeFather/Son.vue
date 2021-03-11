6bfb2887397936be5e64bb099f1b63c738fef870
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-03-11 08:57:43 +0800

    add 子组件改变父组件。

--- src/views/life/SonChangeFather/Son.vue
+++ src/views/life/SonChangeFather/Son.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="Son">
     <p>Son</p>
+    <button @click="_cFatherData">
+      <span class="blue">改变</span>
+      父组件的data
+      <span class="blue">父组件未在template</span>
+      上使用 父组件<span class="red">不更新</span>
+    </button>
+    <br />
+    <button @click="_cFatherDataTem">
+      <span class="blue">改变</span>
+      父组件的data
+      <span class="blue">父组件在template</span>
+      上使用 父组件<span class="red">更新</span>
+    </button>
     <GrandSon />
   </div>
 </template>
@@ -47,7 +60,12 @@ export default {
     console.log(`%c       Son-->8、destroyed`, "color:#CE0000");
   },
   methods: {
-    aa() {}
+    _cFatherData() {
+      this.$emit("update:fatherDataF", "bbb");
+    },
+    _cFatherDataTem() {
+      this.$emit("update:fatherDataTemplateF", "ddd");
+    }
   }
 };
 </script>
--- src/views/life/SonChangeFather/SonChangeFather.vue
+++ src/views/life/SonChangeFather/SonChangeFather.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="SonChangeFather">
     <p>SonChansgeFather</p>
-    <Son />
+    <Son
+      :fatherDataF.sync="fatherData"
+      :fatherDataTemplateF.sync="fatherDataTemplate"
+    />
+    <p>父组件的值：{{ fatherDataTemplate }}</p>
   </div>
 </template>
 
@@ -20,10 +24,46 @@ export default {
   computed: {
     // ...mapState(["activityData"])
   },
-  data() {
-    return {};
+  watch: {
+    fatherData: function(newvalue, oldvalue) {
+      console.info("old-fatherData=", oldvalue);
+      console.info("new-fatherData=", newvalue);
+    },
+    fatherDataTemplate: function(newvalue, oldvalue) {
+      console.info("old-fatherDataTemplate=", oldvalue);
+      console.info("new-fatherDataTemplate=", newvalue);
+    }
   },
-  created() {},
+  data() {
+    return {
+      fatherData: "aaa",
+      fatherDataTemplate: "ccc"
+    };
+  },
+  beforeCreate() {
+    console.log(`%cFather-->1、beforeCreate`, "color:#FF9797");
+  },
+  created() {
+    console.log(`%cFather-->2、created`, "color:#FF9797");
+  },
+  beforeMount() {
+    console.log(`%cFather-->3、beforeMount`, "color:#FF9797");
+  },
+  mounted() {
+    console.log(`%cFather-->4、mounted`, "color:#FF9797");
+  },
+  beforeUpdate() {
+    console.log(`%cFather-->5、beforeUpdate`, "color:#FF9797");
+  },
+  updated() {
+    console.log(`%cFather-->6、updated`, "color:#FF9797");
+  },
+  beforeDestroy() {
+    console.log(`%cFather-->7、beforeDestroy`, "color:#FF9797");
+  },
+  destroyed() {
+    console.log(`%cFather-->8、destroyed`, "color:#FF9797");
+  },
   methods: {
     aa() {}
   }
