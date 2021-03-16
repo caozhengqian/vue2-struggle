dbcb865bc42d737a6644475b6d059151a83632d1
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-03-16 19:11:16 +0800

    add 添加子组件

--- src/views/life/BigObj/BigObj.vue
+++ src/views/life/BigObj/BigObj.vue
@@ -1,8 +1,32 @@
 <template>
   <div class="BigObj">
     <p>BigObj</p>
-    <button @click="_cStr"><span class="blue">不传</span>给子组件</button>
-    <span>{{ str }}</span>
+    <button @click="_cStr">改变<span class="blue">string</span></button>
+    {{ str }}
+    <br />
+    <br />
+    <button @click="_cList1">改变<span class="blue">list</span></button>
+    <span class="red">this.list2 = this.list.push('c');每次都更新</span>
+    <br />
+    {{ JSON.stringify(list) }}
+    <br />
+    <br />
+    <button @click="_cListPush">改变<span class="blue">list2</span></button>
+    <span class="red"
+      >this.list2.<span class="blue">push</span>('c');每次都更新</span
+    >
+    <br />
+    {{ JSON.stringify(list2) }}
+    <br />
+    <br />
+    <button @click="_cListReverse">
+      改变<span class="blue">listReverse</span>
+    </button>
+    <span class="red"
+      >this.list2.<span class="blue">reverse</span>;每次都更新</span
+    >
+    <br />
+    {{ JSON.stringify(listReverse) }}
     <Son13 />
   </div>
 </template>
@@ -26,6 +50,8 @@ export default {
     return {
       str: "aaa",
       list: ["a", "b"],
+      list2: ["a", "b"],
+      listReverse: ["a", "b"],
       obj: { aKey: "aVal", bKey: "bVal" },
       objInList: ["a", { aKey: "aVal" }],
       listInObj: { a: ["b", "c"] }
@@ -57,9 +83,16 @@ export default {
   },
   methods: {
     _cStr() {
-      // this.str === "aaa" ? (this.str = "bbb") : (this.str = "aaa");
-      // this.str = this.str.substr(0, 1);
-      this.str = this.str.concat("");
+      this.str === "aaa" ? (this.str = "bbb") : (this.str = "aaa");
+    },
+    _cList1() {
+      this.list = ["b", "c"];
+    },
+    _cListPush() {
+      this.list2.push("c");
+    },
+    _cListReverse() {
+      this.listReverse.reverse();
     }
   }
 };
